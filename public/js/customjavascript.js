d456ce07c7a9916e1d0e550896a0bc5ad0c654c7
Alex Ryan <thomas.alexander.ryan@gmail.com>
2014-12-06 13:50:12 -0500

    Initial final draft of site

--- public/js/customjavascript.js
+++ public/js/customjavascript.js
@@ -1,5 +1,27 @@
 $(document).ready(function () {
     
+    
+	jQuery('.postFadeIn').addClass("hiddentwo").viewportChecker({
+	    classToAdd: 'visibletwo animated fadeIn', // Class to add to the elements when they are visible
+	    offset: 300,
+    }); 
+    
+    jQuery('.postFadeIn2').addClass("hiddentwo").viewportChecker({
+        classToAdd: 'visibletwo animated fadeIn',
+        offset: 400,
+    });
+    
+    jQuery('.postPulse').addClass("hiddentwo").viewportChecker({
+        classToAdd: 'visibletwo animated pulse',
+        offset: 5000,
+    });
+    
+    jQuery('.postRubber').addClass("hiddentwo").viewportChecker({
+        classToAdd: 'visibletwo animated rubberBand',
+        offset: 300,
+    });
+    
+    
     $(".outsidelink").click(function () {
         window.open($(this).find("a:first").attr("href"));
         return false;
